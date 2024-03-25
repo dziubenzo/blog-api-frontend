4516650c0bdd88bae11696fd1f9e9ea7811f91e1
dziubenzo <dziubenzo@gmail.com>
2024-03-25 17:48:51 +0100

    Add like/unlike functionality to posts
    A huge commit, but it had several intertwined parts involved. The
    functionality is 100% complete, including localStorage and preventing
    rerender delay due to API requests.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,6 +1,12 @@
 import { useOutletContext, useParams } from 'react-router-dom';
 import { formatDistanceToNow } from 'date-fns';
 import Comment from './Comment';
+import { useState } from 'react';
+import {
+  addPostToLocalStorage,
+  checkIfPostLiked,
+  removePostFromLocalStorage,
+} from '../helpers';
 
 function Post() {
   // Get post slug
@@ -9,23 +15,80 @@ function Post() {
   // Get post
   const [post] = posts.filter((post) => post.slug === postSlug);
   // Destructure post
-  const { title, content, author, create_date, likes } = post;
+  const { title, content, author, create_date } = post;
   // Get post comments
   const postComments = comments.filter((comment) => {
     return comment.post == post._id;
   });
 
+  // State for updating post likes count dynamically
+  const [postLikes, setPostLikes] = useState(post.likes);
+  // State for determining whether or not the post is already liked
+  const [isPostLiked, setIsPostLiked] = useState(checkIfPostLiked(post._id));
+  // State for disabling Like/Unlike button until after the component is rerendered
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
+  // Render all comments
   function renderComments() {
     return postComments.map((comment) => {
       return <Comment key={comment._id} comment={comment} />;
     });
   }
 
+  // Like post
+  async function likePost(event) {
+    event.preventDefault();
+    try {
+      // Disable button
+      setIsButtonDisabled(true);
+      await fetch(`http://localhost:3000/posts/${post._id}/like`, {
+        method: 'PUT',
+      });
+      // Update both state and the posts object so that the main page is in sync without refresh
+      setPostLikes(postLikes + 1);
+      post.likes++;
+      // Update localStorage and state
+      addPostToLocalStorage(post._id);
+      setIsPostLiked(true);
+      setIsButtonDisabled(false);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  // Unlike post
+  async function unlikePost(event) {
+    event.preventDefault();
+    try {
+      // Disable button
+      setIsButtonDisabled(true);
+      await fetch(`http://localhost:3000/posts/${post._id}/unlike`, {
+        method: 'PUT',
+      });
+      // Update both state and the posts object so that the main page is in sync without refresh
+      setPostLikes(postLikes - 1);
+      post.likes--;
+      // Update localStorage and state
+      removePostFromLocalStorage(post._id);
+      setIsPostLiked(false);
+      setIsButtonDisabled(false);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
   return (
     <div className="post-wrapper">
       <div className="post-info-wrapper">
         <div className="like-post">
-          <img src="likes.svg" alt="Like Icon - Post" />
+          <form method="post" onSubmit={isPostLiked ? unlikePost : likePost}>
+            <button type="submit" disabled={isButtonDisabled}>
+              <img
+                src={isPostLiked ? 'liked.svg' : 'likes.svg'}
+                alt={isPostLiked ? 'Unlike Icon - Post' : 'Like Icon - Post'}
+              />
+            </button>
+          </form>
         </div>
         <div className="post-info">
           <p className="post-author">{author}</p>
@@ -33,7 +96,7 @@ function Post() {
             {formatDistanceToNow(create_date, { addSuffix: true })}
           </p>
           <p className="post-likes">
-            {likes} {likes === 1 ? 'like' : 'likes'}
+            {postLikes} {postLikes === 1 ? 'like' : 'likes'}
           </p>
         </div>
       </div>
--- src/helpers.jsx
+++ src/helpers.jsx
@@ -17,3 +17,30 @@ export async function dataLoader() {
     return error;
   }
 }
+
+// Check localStorage for whether or not a post is liked
+export function checkIfPostLiked(postId) {
+  // Create an empty array if it is the first visit to the post page
+  if (!localStorage.getItem('likedPosts')) {
+    localStorage.setItem('likedPosts', JSON.stringify([]));
+    return false;
+  }
+  // Return true if post id is in it
+  // Return false if it isn't
+  const likedPosts = JSON.parse(localStorage.getItem('likedPosts'));
+  return likedPosts.includes(postId) ? true : false;
+}
+
+// Add liked post to localStorage (like post action)
+export function addPostToLocalStorage(postId) {
+  const likedPosts = JSON.parse(localStorage.getItem('likedPosts'));
+  likedPosts.push(postId);
+  localStorage.setItem('likedPosts', JSON.stringify(likedPosts));
+}
+
+// Remove liked post from localStorage (unlike post action)
+export function removePostFromLocalStorage(postId) {
+  const likedPosts = JSON.parse(localStorage.getItem('likedPosts'));
+  const newLikedPosts = likedPosts.filter((storedId) => storedId !== postId);
+  localStorage.setItem('likedPosts', JSON.stringify(newLikedPosts));
+}
